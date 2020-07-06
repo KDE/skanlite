24c139608d473e65d20ec8dbcef22991ecdfc7bd
Pino Toscano <pino@kde.org>
2020-07-06 16:43:17 +0200

    Fix version handling
    There are 2 "version.h" generated headers: one static in the sources
    filled by cmake, and the other generated by ecm_setup_version().
    
    Consolidate and port to ecm_setup_version():
    - move SKANLITE_VERSION_STRING up in the CMakeLists.txt
    - use SKANLITE_VERSION_STRING in ecm_setup_version() instead of the KF5
      requirement
    - generate src/skanlite_version.h
    - drop the PACKAGE_VERSION_FILE stuff, as it is a cmake config file for
      exported libraries (there are none)
    - port skanlite to skanlite_version.h and the SKANLITE_VERSION_* macros
      it provides
    - remove the version.h.cmake template, no more needed now

--- src/main.cpp
+++ src/main.cpp
@@ -30,9 +30,9 @@
 #include <Kdelibs4ConfigMigrator>
 
 #include "skanlite.h"
-#include "version.h"
 
 #include <skanlite_debug.h>
+#include <skanlite_version.h>
 
 int main(int argc, char *argv[])
 {
@@ -47,7 +47,7 @@ int main(int argc, char *argv[])
 
     KAboutData aboutData(QLatin1String("Skanlite"), // componentName, k4: appName
                          i18n("Skanlite"), // displayName, k4: programName
-                         QLatin1String(skanlite_version), // version
+                         QLatin1String(SKANLITE_VERSION_STRING), // version
                          i18n("Scanning application by KDE based on libksane."), // shortDescription
                          KAboutLicense::GPL, // licenseType
                          i18n("(C) 2008-2020 Kåre Särs"), // copyrightStatement
